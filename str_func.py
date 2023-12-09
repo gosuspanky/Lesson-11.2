6f5704951f0702e5fa0359ac53adc3bd0ae299bc
Arthur Gaetov <agaetov@gmail.com>
2023-12-10 00:02:30 +0300

    update str_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def caps(string):
+    """
+    Конфликтующее описание =)
+    """
     return string.upper()
+
+
+def title(string):
+    """
+    Возвращает каждую буквы строки заглавной
+    """
+    return string.title()
